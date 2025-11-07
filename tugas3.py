074ddb0fab18cc3e6eba6a4a2a4534d02b8585a2
mirarachma <mirarpd@student.upi.edu>
2025-11-07 09:28:42 +0700

    Menambah perubahan di tugas3.py

--- tugas3.py
+++ tugas3.py
@@ -6,8 +6,16 @@ Mira
 
 bilangan = int(input("Masukkan bilangan = "))
 
+jumlah_genap = 0
+jumlah_ganjil = 0
+
 for b in range(1, bilangan + 1):
     if b % 2 == 0:
         print(f"{b} adalah bilangan genap")
+        jumlah_genap += 1
     else:
         print(f"{b} adalah bilangan ganjil")
+        jumlah_ganjil += 1
+
+print("\nJumlah bilangan genap:", jumlah_genap)
+print("\nJumlah bilangan ganjil:", jumlah_ganjil)
