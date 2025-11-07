129810f2765afc6abb2dab30bf0cca0ed29bb5d2
mirarachma <mirarpd@student.upi.edu>
2025-11-07 09:21:28 +0700

    Menambah perubahan di tugas1.py

--- tugas1.py
+++ tugas1.py
@@ -7,5 +7,8 @@ Mira
 for i in range(1, 51):
     if i % 5 == 0:
         print("pass")
+        count_pass += 1
     else:
         print(i, end=" ")
+
+print("\nJumlah 'pass' muncul sebanyak:", count_pass)
